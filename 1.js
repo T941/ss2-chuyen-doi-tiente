216237dd9a21efbf11a28f7ede3867dd20e22d47
tranquocsang <tranquocsang19977@gmail.com>
2019-06-28 10:59:08 +0700

    baitap

---
+++ 1.js
@@ -0,0 +1,21 @@
+function doitien(){
+let a = parseInt(document.getElementById("cot1").value);
+let b = parseInt(document.getElementById("cot2").value);
+let c = parseInt(document.getElementById("cot3").value);
+let d = parseInt(document.getElementById("tien").value)
+if (a === 1 && b === 2) {
+
+ alert("Kết quả = " +(d = c/23000))
+
+}
+ if(a === 1 && b === 1) {
+  alert("Kết quả = " +c)
+ }
+
+ if(a === 2 && b === 1){
+  alert("Kết quả = " +(d=c*23000))
+ }
+ if (a===2 && b===2){
+  alert("Kết quả = " +c)
+ }}
+
